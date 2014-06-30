ed94b8f277cabce8fea0c7295c592c091cfffd6b
JenGates3255 <Jengates3255@gmail.com>
2014-06-30 11:00:24 -0600

    In class

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).on('ready', function() {
+  
+var stopWatch = function (hours, mintues, seconds){
+
+	if (!hours){
+		hours = 0;
+	}
+	if (!minutes){
+		minutes = 0;
+	}
+	if (!seconds){
+		seconds = 0;
+	}
+	console.log(hours, minutes, seconds)
+};
+
+
+
+
+});
